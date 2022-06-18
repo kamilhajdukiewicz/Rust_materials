57f98415b770adfa7c2d60525a05998c4a357f9a
Kamil Hajdukiewicz <hk39513@zut.edu.pl>
2022-06-18 20:18:28 +0200

    Init

---
+++ data_types_sample/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    //uint and int types
+    let int_32_sample1 : i32 = 32i32;
+    let uint_32_sample1 : u32 = 32;
+    let uint_32_sample2 = 32_000u32;
+
+    println!("Int 32 sample 1 = {}", int_32_sample1);
+    println!("UInt 32 sample 1 = {}", uint_32_sample1);
+    println!("UInt 32 sample 2 = {}", uint_32_sample2);
+
+    //float types
+    let float_32_sample : f32 = 32.6;
+    let float_64_sample : f64 = 32.5;
+
+    println!("Float 32 sample = {}", float_32_sample);
+    println!("Float 64 sample = {}", float_64_sample);
+
+    //bool types
+    
+}
