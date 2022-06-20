76bde5a9a6ec4cf79fe3412b531f5e2b31471d52
Kamil Hajdukiewicz <kamil.hajdukiewicz@codelab.eu>
2022-06-20 20:01:14 +0200

    Updated functions sample

--- functions_sample/src/main.rs
+++ functions_sample/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     println!("Hello, world!");
     another_function();
     function_with_parameters(10, 20);
+
+    println!("{}", get_five());
+    println!("{}", get_five_with_return());
 }
 
 //There is no difference if the function is before or after main
@@ -12,4 +15,15 @@ fn another_function() {
 fn function_with_parameters(x: i32, y: i32) {
     println!("First parameter = {}", x);
     println!("Second parameter = {}", y);
+}
+
+//functions which returns value
+//if no 'return' -> no semicolon at the end
+fn get_five() -> i32 {
+    5
+}
+
+//if 'return' -> semicolon at the end
+fn get_five_with_return() -> i32 {
+    return 5;
 }
