dfc833de3a9b4f0dfed7a8e90361df05de65f84e
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-06-22 22:51:37 +0200

    Added sample with multiple mutable references

--- reference_sample/src/main.rs
+++ reference_sample/src/main.rs
@@ -10,6 +10,22 @@ fn main() {
     println!("Text before the change = {}", mut_str);
     change_text(&mut mut_str);
     println!("Text after change = {}", mut_str);
+
+    //----This code below is not allowed---
+    //you can have only one mutable reference to a particular piece of data at a time.
+    //The benefit of having this restriction is that Rust can prevent data races at compile time
+    //let s1 = &mut mut_str;
+    //let s2 = &mut mut_str;
+    //println!("s1 {}, s2 {}", s1, s2);
+
+    //But this is allowed, cause the value can't be modified by s1 or s2
+    let s1 = &mut_str;
+    let s2 = &mut_str;
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    let s3 = &mut mut_str;
+    println!("s3 = {}", s3);
+
 }
 
 fn calculate_length(s: &String) -> usize {  //the function takes the reference. str is still owner.
