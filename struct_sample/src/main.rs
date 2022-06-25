65c25382a72f276fc2c50d4422b5037ce4d55b26
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-06-25 14:41:47 +0200

    Created mutable instance

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -1,14 +1,18 @@
 fn main() {
 
     //instance of the user struct
-    let user1 = User {
+    let mut user1 = User {
         active: false,
         username: String::from("RagnarTrump"),
         email: String::from("myemail@gmail.com"),
         sign_in_count: 0,
     };
 
-    println!("Hello, world!");
+    println!("Username before change: {}", user1.username);
+
+    user1.username = String::from("NewUsername");
+
+    println!("Username after change: {}", user1.username);
 }
 
 struct User {
