cfe546ed01c9060143b28c27b575a30a897adf00
Kamil Hajdukiewicz <hk39513@zut.edu.pl>
2022-06-25 20:45:59 +0200

    Created program which calculate area of the rectangle

--- struct_sample_program/src/main.rs
+++ struct_sample_program/src/main.rs
@@ -1,3 +1,23 @@
+//Added attribute which allows you to print struct in println!
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rec = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    //Different ways to print rec value
+    println!("Rect1 is {:#?}", rec);
+    println!("Rect1 is {:?}", rec);
+    dbg!(&rec);
+    println!("The area of the rectangle is {} square pixels.", area(&rec));
+}
+
+fn area(rec: &Rectangle) -> u32 {
+    rec.width * rec.height
 }
