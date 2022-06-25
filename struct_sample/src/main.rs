1bd3dabb224df4536d204e53d012e293849f8ef3
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-06-25 14:48:52 +0200

    Added builder for user

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -13,6 +13,9 @@ fn main() {
     user1.username = String::from("NewUsername");
 
     println!("Username after change: {}", user1.username);
+
+    let user2 = build_user(String::from("User2"), String::from("user2@gmail.com"));
+    println!("User2 username: {}, email: {}", user2.username, user2.email);
 }
 
 struct User {
@@ -20,4 +23,15 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+}
+
+//If the parameter have the same name as the member of the struct
+//You can write only the name -> not username: username
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 0,
+    }
 }
