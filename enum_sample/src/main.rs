0464a270594a7e6229da22a803fe0a15447ec547
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-06-26 18:37:48 +0200

    Updated enum sample

--- enum_sample/src/main.rs
+++ enum_sample/src/main.rs
@@ -1,8 +1,33 @@
+#[derive(Debug)]
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("Message info: {:#?}", self);
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let ip1 = IpAddrKind::V4(String::from("localhost"));
+    println!("IP1 = {:#?}", ip1);
+
+    let message1 = Message::Quit;
+    let message2 = Message::Move {
+        x: 10,
+        y: 20,
+    };
+
+    message1.call();
+    message2.call();
 }
