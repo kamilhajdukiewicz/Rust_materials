04601ebfc8c5e9dac477ec6ff84e5ea07008b44a
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-06-30 23:49:18 +0200

    Modified modules. Some methods are now public

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,8 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();    
+    }
+
+    fn cook_order() {}
+}
+
+fn deliver_order() {
+
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
