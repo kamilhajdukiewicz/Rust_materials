736952968a165c2480e96aafef257cc280cb778b
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-07-03 12:47:00 +0200

    Added use sample

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -10,16 +10,24 @@ mod back_of_house {
         super::deliver_order();    
     }
 
-    fn cook_order() {}
+    fn cook_order() {
+        //it's not possible even with use
+        //hosting::add_to_waitlist();
+    }
 }
 
 fn deliver_order() {
 
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
+
+    //with use crate::front_of_house::hosting is possible
+    hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
