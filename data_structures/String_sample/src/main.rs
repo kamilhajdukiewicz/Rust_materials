b627f212661de0183be00a9532bca965976f4595
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-07-06 18:55:51 +0200

    Updating a sample with strings

--- data_structures/String_sample/src/main.rs
+++ data_structures/String_sample/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+   
+    //creating a String variable
+    let mut str1 : String = String::new();
+
+    let data = "Data for String";
+
+    //fill the String with some text data
+    let str2 = data.to_string();
+    let str3 = String::from(data);
+
+    println!("1. {}", str2);
+    println!("2. {}", str3);
+
+    //updating a String with string
+    str1.push_str("Hello");
+    println!("Updated string: {}", str1);
+
+    //updating a string with character
+    str1.push('!');
+    println!("After push: {}", str1);
 }
