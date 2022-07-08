323a0d9818f8ade930cca48c42958a45f8dd8013
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-07-08 22:49:52 +0200

    Sample usage of hash map

--- data_structures/hash_map/hash_map/src/main.rs
+++ data_structures/hash_map/hash_map/src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+
+    //creating a hash map
+    let mut hm = HashMap::new();
+
+    //adding elements to hash map
+    hm.insert(String::from("Yellow"), 10);
+    hm.insert(String::from("Red"), 50);
+
+    //creating a hash map using vectors
+    let teams = vec![String::from("Blue"), String::from("Green")];
+    let points = vec![10, 50];
+
+    let mut hm2 : HashMap<_ , _> = teams.into_iter().zip(points.iter()).collect();
+
+    //reading data from hash map
+    match hm.get(&String::from("Yellow")) {
+        Some(point) => println!("Yellow team points = {}", point),
+        None => println!("Yellow team doesn't have points"),
+    }
+    
+    //iterating through all elements
+    for (key, value) in hm2 {
+        println!("{} team points = {}", key, value);
+    }
+
+
+
 }
