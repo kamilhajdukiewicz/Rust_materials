692b1627e3e6b1c0e696727ec2e980c44b337ddd
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-07-11 19:40:52 +0200

    Created a project for error_handling samples

--- data_structures/sample_algorithms/src/main.rs
+++ data_structures/sample_algorithms/src/main.rs
@@ -55,3 +55,9 @@ fn string_to_pig_latin(str : &mut String) {
         str.remove(0);
     }
 }
+
+
+//3.
+//Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. 
+//For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people 
+//in a department or all people in the company by department, sorted alphabetically.
