f705778a5eb9c57adbcc4916b3d9fcd36edffce3
Kamil Hajdukiewicz <kamhaj97@gmail.com>
2022-07-13 20:54:23 +0200

    Added sample with recoverable error

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,4 +1,11 @@
+use std::fs::File;
+
 fn main() {
     //RUST_BACKTRACE=1 cargo run to see callstack 
-    panic!("Crash!");
+    //panic!("Crash!");
+
+    let f = match File::open("hello.txt") {
+        Ok(file) => file,
+        Err(error) => panic!("File cannot be opened: {:#?}", error),
+    };
 }
